6183ddb22a52cc29f4bddf0d1b7b6c85314e878c
Karina Bora <96630595+karinabora@users.noreply.github.com>
2021-12-29 09:52:13 +0530

    Add files via upload

---
+++ String Format.py
@@ -0,0 +1,10 @@
+# b = "Hello, World!"
+# print(b[2:5])
+
+# txt = "The best things in life are free!"
+# if "free" in txt:
+#   print("Yes, 'free' is present.")
+
+# txt = "The best things in life are free!"
+# if "expensive" not in txt:
+#   print("No, 'expensive' is NOT present.")
